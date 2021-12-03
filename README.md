1919be5e2653a9266db0e7f3709aeffb776b9c06
giovani.alves <giovani.alves.glv@gmail.com>
2021-12-03 15:46:05 -0300

    A função closeModal foi inserida no botão de cancelar

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Pizzas Purchase Project
 
-### Construindo a interface de um sistema de venda de pizzas com HTML, CSS e JavaScript.
+### Construindo a interface de um sistema de venda de pizzas com HTML, CSS e JavaScript. Construído durante o curso de JavaScript do B7W.
--- script.js
+++ script.js
@@ -1,22 +1,56 @@
 const select = el => document.querySelector(el)
 const selectAll = el => document.querySelectorAll(el)
 
+let modalQtd = 1
+
+//Listando as pizzas
 pizzaJson.map((item, index) => {
    let pizzaItem = select('.models .pizza-item').cloneNode(true) //Clonando o item
 
+   pizzaItem.setAttribute('key', index)
    pizzaItem.querySelector('.pizza-item--img img').src = item.img
    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
+
    pizzaItem.querySelector('a').addEventListener('click', (event) => {
       event.preventDefault()
+      
+      let key = event.target.closest('.pizza-item').getAttribute('key');
+      modalQtd = 1;
+
+      select('.pizzaBig img').src = pizzaJson[key].img
+      select('.pizzaInfo h1').innerHTML = pizzaJson[key].name
+      select('.pizzaInfo--desc').innerHTML = pizzaJson[key].description
+
+      selectAll('.pizzaInfo--size').forEach((size, sizeIndex) => {
+         if(sizeIndex === 2){
+            size.classList.add('selected')
+         }
+         size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex]
+      })
+
+      select('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`
+
 
       select('.pizzaWindowArea').style.opacity = 0
       select('.pizzaWindowArea').style.display = 'flex'
-      setInterval( () => {
+      setInterval(() => {
          select('.pizzaWindowArea').style.opacity = 1
       }, 200)
    })
 
    select('.pizza-area').append(pizzaItem) //Adiciona mais um conteúdo no DOM final
 })
+
+//Eventos do modal
+selectAll('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item) => {
+   item.addEventListener('click', closeModal)
+})
+
+function closeModal() {
+   select('.pizzaWindowArea').style.opacity = 0
+   setTimeout(() => {
+      select('.pizzaWindowArea').style.display = 'none'
+   }, 200)
+}
