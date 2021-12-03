711f6f1ee1bed5ce1a1120c9940af70ddab6db42
giovani.alves <giovani.alves.glv@gmail.com>
2021-12-03 14:36:06 -0300

    Foi adicionado event de click para mostrar o modal de compra das pizzas

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# pizza_purchase_project
+# Pizzas Purchase Project
+
+### Construindo a interface de um sistema de venda de pizzas com HTML, CSS e JavaScript.
--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-const querySelect = el => document.querySelector(el)
-const querySelectAll = el => document.querySelectorAll(el)
+const select = el => document.querySelector(el)
+const selectAll = el => document.querySelectorAll(el)
 
 pizzaJson.map((item, index) => {
-   let pizzaItem = querySelect('.models .pizza-item').cloneNode(true) //Clonando o item
+   let pizzaItem = select('.models .pizza-item').cloneNode(true) //Clonando o item
 
    pizzaItem.querySelector('.pizza-item--img img').src = item.img
    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
+   pizzaItem.querySelector('a').addEventListener('click', (event) => {
+      event.preventDefault()
 
-   querySelect('.pizza-area').append(pizzaItem) //Adicionar mais um conteúdo no DOM final
+      select('.pizzaWindowArea').style.opacity = 0
+      select('.pizzaWindowArea').style.display = 'flex'
+      setInterval( () => {
+         select('.pizzaWindowArea').style.opacity = 1
+      }, 200)
+   })
+
+   select('.pizza-area').append(pizzaItem) //Adiciona mais um conteúdo no DOM final
 })
