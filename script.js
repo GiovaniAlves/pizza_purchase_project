9994a5cc8202a02e63f987d60d167ca6d5ed0ec6
giovani.alves <giovani.alves.glv@gmail.com>
2021-12-03 17:31:40 -0300

    O addEventListener foi adicionado ao botão do carinho

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const select = el => document.querySelector(el)
 const selectAll = el => document.querySelectorAll(el)
 
+let cart = []
 let modalQtd = 1
+let modalKey = 0
 
 //Listando as pizzas
 pizzaJson.map((item, index) => {
@@ -16,13 +18,16 @@ pizzaJson.map((item, index) => {
    pizzaItem.querySelector('a').addEventListener('click', (event) => {
       event.preventDefault()
       
-      let key = event.target.closest('.pizza-item').getAttribute('key');
-      modalQtd = 1;
+      let key = event.target.closest('.pizza-item').getAttribute('key')
+      modalKey = key
+      modalQtd = 1
+      select('.pizzaInfo--qt').innerHTML = 1
 
       select('.pizzaBig img').src = pizzaJson[key].img
       select('.pizzaInfo h1').innerHTML = pizzaJson[key].name
       select('.pizzaInfo--desc').innerHTML = pizzaJson[key].description
 
+      select('.pizzaInfo--size.selected').classList.remove('selected')
       selectAll('.pizzaInfo--size').forEach((size, sizeIndex) => {
          if(sizeIndex === 2){
             size.classList.add('selected')
@@ -31,7 +36,6 @@ pizzaJson.map((item, index) => {
       })
 
       select('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`
-
 
       select('.pizzaWindowArea').style.opacity = 0
       select('.pizzaWindowArea').style.display = 'flex'
@@ -44,13 +48,38 @@ pizzaJson.map((item, index) => {
 })
 
 //Eventos do modal
-selectAll('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item) => {
-   item.addEventListener('click', closeModal)
-})
-
 function closeModal() {
    select('.pizzaWindowArea').style.opacity = 0
    setTimeout(() => {
       select('.pizzaWindowArea').style.display = 'none'
    }, 200)
 }
+
+selectAll('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item) => {
+   item.addEventListener('click', closeModal)
+})
+select('.pizzaInfo--qtmenos').addEventListener('click', () => {
+   if(modalQtd > 1){
+      modalQtd --
+      select('.pizzaInfo--qt').innerHTML = modalQtd
+   }
+})
+select('.pizzaInfo--qtmais').addEventListener('click', () => {
+   modalQtd++
+   select('.pizzaInfo--qt').innerHTML = modalQtd
+})
+selectAll('.pizzaInfo--size').forEach((item) => {
+   item.addEventListener('click', () => {
+      select('.pizzaInfo--size.selected').classList.remove('selected')
+      item.classList.add('selected')
+   })
+})
+select('.pizzaInfo--addButton').addEventListener('click', () => {
+   let size = parseInt(select('.pizzaInfo--size.selected').getAttribute('data-key'))
+   cart.push({
+      id: pizzaJson[modalKey].id,
+      size,
+      qtd: modalQtd
+   })
+   closeModal()
+})
